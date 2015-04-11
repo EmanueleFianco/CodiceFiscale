c3aeb4c737c042bd2295c5ba68e20a76276638ff
Gioele Cicchini <gioele.cicchini@gmail.com>
2015-04-11 20:49:13 +0200

    fixes #2 risoluzione issues #2

--- UI/CodFis_Web.php
+++ UI/CodFis_Web.php
@@ -1,16 +1,21 @@
-<html>
-<body>
+<?php
+require_once('../Domain/Persona.class.php');
+require_once('../Domain/CodiceFiscale.class.php');
 
-<form action="../Index.php" method="post">
-Nome: <input type="text" name="Nome"><br>
-Cognome: <input type="text" name="Cognome"><br>
-Data dd/mm/yy <input type="text" name="Data"><br>
-Sesso M/F<input type="text" name="Sesso"><br>
-Provincia<input type="text" name="Provincia"><br>
-Comune<input type="text" name="Comune"><br>
 
-<input type="submit">
-</form>
+$nome=$_POST["Nome"];
+$cognome=$_POST["Cognome"];
+$data=$_POST["Data"];
+$sesso=$_POST["Sesso"];
+$provincia=$_POST["Provincia"];
+$comune=$_POST["Comune"];
 
-</body>
-</html> 
+
+$persona = new Persona($nome,$cognome,$data,$comune,$provincia,$sesso);
+    
+	$codice_fiscale = new CodiceFiscale($persona);
+
+	print "$persona\n$codice_fiscale\n";
+
+
+?>
