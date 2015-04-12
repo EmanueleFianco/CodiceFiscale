5c0c43b1734fd1849e6a3dd57279edbf01c53828
Gioele Cicchini <gioele.cicchini@gmail.com>
2015-04-12 18:52:38 +0200

    inteterfaccia utente piu bella da vedere

--- UI/CodFis_Web.php
+++ UI/CodFis_Web.php
@@ -15,7 +15,45 @@ $persona = new Persona($nome,$cognome,$data,$comune,$provincia,$sesso);
     
 	$codice_fiscale = new CodiceFiscale($persona);
 
-	print "$persona\n$codice_fiscale\n";
+   print "
+   <html>
+		<body bgcolor=".'0099FF'.">
+		<h3 align=center >Risultato</h5>
+   <table>
+	<tr>
+			<td>Nome &nbsp</td>
+			<td>".$persona->getNome()."<br></td>
+	</tr>
+	<tr>
+			<td>Cognome &nbsp</td>
+			<td>".$persona->getCognome()."<br></td>
+	</tr>
+	<tr>
+			<td>Data dd/mm/yy &nbsp </td>
+			<td>".$persona->getNascita()->format('d/m/Y')."<br></td>
+	</tr>
+	<tr>
+			<td>Sesso M/F &nbsp</td>
+			<td>".$persona->getSesso()."<br></td>
+	</tr>
+	<tr>
+			<td>Provincia &nbsp</td>
+			<td>".$persona->getComune()->getProvincia()."<br></td>
+	</tr>
+	<tr>
+			<td>Comune &nbsp</td>
+			<td>".$persona->getComune()->getCitta()."<br></td>
+	</tr>
+	<tr>
+			<td>Codice Fiscale &nbsp</td>
+			<td>".$codice_fiscale->getCodice()."<br></td>
+	</tr>
+	</table>
+	</body>
+	</html>
+		";
+
+	
 
 
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <html>
-<body>
+<body bgcolor="0099FF">
 
+<h3 align=center >Calcolo Codice Fiscale</h5>
 <form action="UI/CodFis_Web.php" method="post">
-Nome: <input type="text" name="Nome"><br>
-Cognome: <input type="text" name="Cognome"><br>
-Data dd/mm/yy <input type="text" name="Data"><br>
-Sesso M/F<input type="text" name="Sesso"><br>
-Provincia<input type="text" name="Provincia"><br>
-Comune<input type="text" name="Comune"><br>
 
-<input type="submit">
+<table border="0">
+	<tr>
+			<td>Nome</td>
+			<td><input type="text" name="Nome"><br></td>
+	</tr>
+	<tr>
+			<td>Cognome</td>
+			<td><input type="text" name="Cognome"><br></td>
+	</tr>
+	<tr>
+			<td>Data dd/mm/yy</td>
+			<td><input type="text" name="Data"><br></td>
+	</tr>
+	<tr>
+			<td>Sesso M/F</td>
+			<td><input type="text" name="Sesso"><br></td>
+	</tr>
+	<tr>
+			<td>Provincia</td>
+			<td><input type="text" name="Provincia"><br></td>
+	</tr>
+	<tr>
+			<td>Comune</td>
+			<td><input type="text" name="Comune"><br></td>
+	</tr>
+
+ 	<tr>
+ 	<td align =center><input type="submit"></td>
+ 	</tr>
+ 
+</table>
 </form>
 
 </body>
